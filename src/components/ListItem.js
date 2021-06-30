a17dc4620497a9a5bd21beb8b69671ccbb29bf95
scredi <maxime.petronio@wibilong.com>
2021-06-30 20:51:33 +0200

    add heart icon + red color & small css fixes

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -12,7 +12,7 @@ const ListItem = ({id, username, picture, isLiked, likeUser}) => {
     <div className={like ? "list-item liked" : "list-item"}>
       <img alt="" src={picture}/>
       <div className="list-item__username" onClick={handleLikeUser}>
-        {username}
+        <span>{username}</span>
       </div>
     </div>
   );
