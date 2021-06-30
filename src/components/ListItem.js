3b38841c7c96c8a7c803c0d364457396d5861ad5
scredi <maxime.petronio@wibilong.com>
2021-06-30 20:02:02 +0200

    test like state on ListItem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,8 +1,16 @@
+import React, { useState } from 'react';
+
 const ListItem = ({username, picture}) => {
+  const [like, setLike] = useState(false);
+
+  const handleLikeUser = () => {
+    setLike(!like);
+  }
+
   return (
-    <div className="list-item">
+    <div className={like ? "list-item liked" : "list-item"}>
       <img alt="" src={picture}/>
-      <div className="list-item__username">{username}</div>
+      <div className="list-item__username" onClick={() => handleLikeUser()}>{username}</div>
     </div>
   );
 }
