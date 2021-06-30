44a47b944ad6f3e129e256536097d324d188f37a
scredi <maxime.petronio@wibilong.com>
2021-06-30 19:33:33 +0200

    test ListItem

--- src/App.js
+++ src/App.js
@@ -4,9 +4,48 @@ import InputSearch from './components/InputSearch';
 import Loader from './components/Loader';
 
 const App = () => {
+  const [data, setData] = useState([])
+  , [isLoading, setLoading] = useState(true);
+
+  const getData = async () => {
+    const response = await fetch('./dummy.json',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      }
+    );
+
+    if (!response.ok) {
+      console.log(response.status);
+    }
+
+    const json = await response.json();
+
+    setTimeout(() => {
+      setLoading(false);
+      setData(json);
+    }, 1500)
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
   return (
     <div className="App">
-      <Loader />
+      { isLoading && <Loader /> }
+      <div className="list-container">
+        {
+          data.length > 0
+          && data.map(item => (
+            <ListItem
+              {...item}
+            />
+          )) 
+        }
+      </div>
     </div>
   );
 }
---
+++ src/components/ListItem.js
@@ -0,0 +1,9 @@
+const ListItem = ({username}) => {
+  return (
+    <div className="list-item">
+      <div>{username}</div>
+    </div>
+  );
+}
+
+export default ListItem;
